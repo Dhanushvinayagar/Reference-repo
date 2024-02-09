ae39c38a9d8d850db0c458f2580be7d44bc7049d
Dhanushvinayagar <dhanush22092002@gmail.com>
2024-02-09 16:44:59 +0530

    Updated .env package.json

--- vite-env/package.json.js
+++ vite-env/package.json.js
@@ -3,7 +3,12 @@ const file =
         {
             "scripts": {
                 "dev": "vite",
-                "build": "tsc && vite build",   //this always runs for production env whild build
+
+                "build": "tsc && vite build",  
+                //if only .env file is present without other file then it is for both production and development
+                //if only .env file and .env.production is present then .env.production is for production and .env is for development
+
+                //if all files are present based on build cmd it works
                 "build:dev": "tsc && vite build --mode development",
                 "build:prod": "tsc && vite build --mode production",
                 "build:stage": "tsc && vite build --mode staging",
